80797ed8d48e0f4595ac28f463bebed94ba8cd18
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-01 11:18:08 -0300

    create astro data tests

--- src/components/home/hero/astroData/astrodata.test.tsx
+++ src/components/home/hero/astroData/astrodata.test.tsx
@@ -1,7 +1,20 @@
-import { formatValue } from "./AstroData";
+import { SolarSystemData, formatValue } from "./AstroData";
+
+import { http } from 'msw' 
+import { setupServer } from 'msw/node';
+
 
 describe("AstroData component", () => {
+    const handlers = [
+        
+    ]
+
+
     it('should formating correctly', () =>{
         expect(formatValue(1000)).toBe("1.000")
     })
+
+    it('should render with api request', () => {
+    }
+)
 })
