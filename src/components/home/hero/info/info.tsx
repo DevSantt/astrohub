5315bde7ef828e540d21a37fb0257351d94b38a6
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-01 09:45:43 -0300

    create info on hero section

---
+++ src/components/home/hero/info/info.tsx
@@ -0,0 +1,45 @@
+import { Reveal } from 'components/utils/reveal/Reveal'
+import styles from './info.module.scss'
+
+import { PiGlobeStandThin, PiCompassToolThin, PiRocketLaunchThin, PiPlanetThin } from 'react-icons/pi'
+
+
+export const Info = () => {
+    return (
+
+        <Reveal>
+
+        <section className={styles.container}>
+            <h2>Explore o Universo</h2>
+
+            <div className={styles.infoContainer}>
+
+                <div className={styles.info}>
+                    <PiGlobeStandThin />
+                    <h3>Publique</h3>
+                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing.</span>
+                </div>
+
+                <div className={styles.info}>
+                    <PiCompassToolThin />
+                    <h3>Cresça</h3>
+                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing.</span>
+                </div>
+
+                <div className={styles.info}>
+                    <PiRocketLaunchThin />
+                    <h3>Aprenda</h3>
+                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing.</span>
+                </div>
+
+                <div className={styles.info}>
+                    <PiPlanetThin />
+                    <h3>Conecte-se</h3>
+                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing.</span>
+                </div>
+
+            </div>
+        </section>
+        </Reveal>
+    )
+}
