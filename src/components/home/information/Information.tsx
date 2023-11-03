4c93941aaeb6fef15c248a21dc8b00e4499ea4b2
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-03 18:19:18 -0300

    update info directories

---
+++ src/components/home/information/Information.tsx
@@ -0,0 +1,11 @@
+import { AstroInfo } from "./astroInfo/AstroInfo";
+import { SolarSystemData } from "./solarSystemData/SolarSystemData";
+
+export const Information = () => {
+    return (
+        <>
+      <AstroInfo/>
+      <SolarSystemData/>
+        </>
+    )
+}
