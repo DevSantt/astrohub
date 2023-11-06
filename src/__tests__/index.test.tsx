5c66022a4870c4d970428fb8339dc4680a5d159e
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-06 12:06:38 -0300

    test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,10 +1,9 @@
-import { render,  } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Main from 'pages/index'
 
 
 describe('Main component', () => {
     it('should render correctly', () => {
-         render(<Main/>);
         
     })
 })
