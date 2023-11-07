ea777f13bf792a2821c2319313b6a8b0e4a4e324
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-07 10:38:49 -0300

    fix jest error

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,5 +1,16 @@
 import { render } from '@testing-library/react'
-import Main from 'pages/index'
+
+beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
+  });
+
 
 
 describe('Main component', () => {
