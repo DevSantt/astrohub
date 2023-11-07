4f814948819f46b7d528308ae161b58a90f73679
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-07 10:38:43 -0300

    fix jest error

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+
+beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
+  });
+
