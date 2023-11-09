a77cae8fdbb6ddff30df0b563178d512cb95205d
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-09 09:26:29 -0300

    add link to button

--- src/components/_ui/button/Button.tsx
+++ src/components/_ui/button/Button.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import style from "./button.module.scss"
 import { Modal } from 'components/_ui/modal/Modal'
+import Link from 'next/link'
 
 interface Props {
     children: string,
-    onClick?: React.MouseEventHandler<HTMLButtonElement>
+    onClick?: React.MouseEventHandler<HTMLButtonElement>,
+    link?: string
 }
 
-export const Button = ({ children, onClick}: Props) => {
+export const Button = ({ children, onClick, link}: Props) => {
 
     return (
     <>
+    <a href={link} target='_blank'>
      <button className={style.button} onClick={onClick}>{children}</button>
+    </a>
     </>
     )
 }
