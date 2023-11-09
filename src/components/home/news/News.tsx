25112620128db7452a895cd56d882d47af244c27
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-09 09:26:50 -0300

    create news section

--- src/components/home/news/News.tsx
+++ src/components/home/news/News.tsx
@@ -1,13 +1,65 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+
 import { newsApi } from 'services/api/apisRequest'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { register } from 'swiper/element/bundle'
+register();
+
+import 'swiper/css/bundle';
+
+
+import styles from './news.module.scss'
+
 export const News = () => {
+   
+
+    const [news, setNews] = useState<any[]>([])
+
     useEffect(() => {
-        newsApi
-            .get('').then(res => console.log(res.data.articles))
+       getNews()
     }, [])
 
+    console.log(news)
+
+    const getNews = () => {
+        newsApi
+        .get('')
+        .then(res => {
+            let data = res.data.articles.filter((articles: any) => (articles.author !== null))
+            setNews(data)
+        })
+        .catch(err => console.error("Ops! Something is wrong: " + err))
+    }
+
+
     return(
-        <></>
+        <>
+           <Swiper 
+                modules={[Navigation, Pagination, Autoplay]}
+                slidesPerView={1}
+                pagination={{clickable: true, dynamicBullets: true }}
+                navigation
+                autoplay={{delay: 5000, disableOnInteraction: true}}
+                className={styles.swiper}
+                loop={true}
+                >
+                    {
+                        news.map((item, index) => {
+                            return (
+                                <SwiperSlide key={index} className={styles.swiperSlide}>
+                                    <img className={styles.image} src={item.urlToImage} alt="" />
+                                    <div className={styles.info}>
+                                        <h2>{item.title}</h2>
+                                        <h3>{item.author}</h3>
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+           </Swiper>
+        </>
     )
 }
