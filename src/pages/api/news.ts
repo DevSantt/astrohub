badadc03f3a7ad1589678880045d70b90776a048
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-18 13:59:15 -0300

    update api

--- src/pages/api/news.ts
+++ src/pages/api/news.ts
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/space', async (req, res) => {
     const SpaceNews = await fetch('https://spacenewsapi-production.up.railway.app/')
     return res.send(SpaceNews)
+})
+
+app.listen(port, () => {
+    console.log('listening')
 })
 
