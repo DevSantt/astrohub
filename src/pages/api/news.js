cb785fa474e50bb4af1075fc289ced9ea5cd6b5b
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-18 14:09:02 -0300

    fetch news

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -1,13 +1,5 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', async (req, res) => {
-    const SpaceNews = await fetch('https://spacenewsapi-production.up.railway.app/')
-    return res.send(SpaceNews)
-})
-
-app.listen(port, () => {
-    console.log('listening')
-})
-
+async function SpaceNews () {
+    const response = await fetch('https://spacenewsapi-production.up.railway.app/')
+    return response.json()
+}
+    
