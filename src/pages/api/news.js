ef3ccc3364f1da36aa9a7f12ebc14f81261e76ba
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-18 14:05:34 -0300

    up endpoint

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/space', async (req, res) => {
+app.get('/', async (req, res) => {
     const SpaceNews = await fetch('https://spacenewsapi-production.up.railway.app/')
     return res.send(SpaceNews)
 })
