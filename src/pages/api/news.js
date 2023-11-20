91bc4b04049faa8c5e02766f748e1b4f7e92d422
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-20 19:52:56 -0300

    update api

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -1,13 +1,13 @@
 async function spaceNews (req, res) {
-   const newsResponse = await fetch("https://spacenewsapi-production.up.railway.app/")
+   const newsResponse = await fetch("https://newsapi-tan.vercel.app/")
    const newsResponseJson = await newsResponse.json()
    const news = await newsResponseJson
-
-   console.log(news)
-   res.send({
-    results: news
-   })
-}
-    
-
-export default spaceNews
+   
+      console.log(news)
+      res.send({
+       results: news
+      })
+   }
+       
+   
+   export default spaceNews
--- src/services/apisRequest.ts
+++ src/services/apisRequest.ts
@@ -4,8 +4,6 @@ export const solarSystemApi = axios.create({
     baseURL: 'https://api.le-systeme-solaire.net/rest'
 })
 
-
-
 export const NewsApi = axios.create({
     baseURL: 'https://astrohub.vercel.app/api/news'
 })
