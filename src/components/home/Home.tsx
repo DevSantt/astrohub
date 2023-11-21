c922bf5455eacc63a68a25e178dae3e59d352dff
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-20 21:11:45 -0300

    add new sections

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,6 +3,8 @@ import { NavBar } from "components/_ui/nav/NavBar";
 import { Hero } from "components/home/hero/Hero";
 import { Information } from "components/home/information/Information";
 import { News } from "./news/News";
+import { Apod } from "./APOD/Apod";
+import { SolarSystemData } from "./solarSystem/SolarSystemData";
 
 export const Home = () => {
     return (
@@ -11,6 +13,8 @@ export const Home = () => {
             <Hero />
             <Information />
             <News />
+            <Apod />
+            <SolarSystemData />
         </>
     )
 }
